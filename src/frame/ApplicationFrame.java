e7c663af3b4ff64699665d597aa7c15648411f65
odanchen <91546290+odanchen@users.noreply.github.com>
2023-05-17 15:02:46 -0400

    fixed static constant fields

--- src/frame/ApplicationFrame.java
+++ src/frame/ApplicationFrame.java
@@ -3,9 +3,9 @@ package frame;
 import javax.swing.*;
 import panels.*;
 
+import java.awt.*;
+
 public class ApplicationFrame extends JFrame {
-    private final JPanel centerPanel = new JPanel();
-    private CustomPanel currentPanel;
     private final CustomPanel loadingPanel = new LoadingPanel(this);
     private final CustomPanel homePanel = new HomePanel(this);
     private final CustomPanel calendarPanel = new CalendarPanel(this);
@@ -14,36 +14,63 @@ public class ApplicationFrame extends JFrame {
     private final CustomPanel seatPanel = new SearchPanel(this);
     private final CustomPanel userInputPanel = new UserInputPanel(this);
     private final CustomPanel exportPanel = new ExportPanel(this);
+    private CustomPanel currentPanel = loadingPanel;
 
     public ApplicationFrame() {
+        super();
+        setLayout(new BorderLayout());
+        JPanel centerPanel = new JPanel();
+        centerPanel.add(loadingPanel);
+        centerPanel.add(homePanel);
+        centerPanel.add(calendarPanel);
+        centerPanel.add(searchPanel);
+        centerPanel.add(flightListPanel);
+        centerPanel.add(seatPanel);
+        centerPanel.add(userInputPanel);
+        centerPanel.add(exportPanel);
+        add(centerPanel, BorderLayout.CENTER);
         setSize(1400, 850);
     }
 
     public void switchToHome() {
-
+        currentPanel.setVisible(false);
+        homePanel.setVisible(true);
+        currentPanel = homePanel;
     }
 
     public void switchToCalendar() {
-
+        currentPanel.setVisible(false);
+        calendarPanel.setVisible(true);
+        currentPanel = calendarPanel;
     }
 
     public void switchToSearch() {
-
+        currentPanel.setVisible(false);
+        searchPanel.setVisible(true);
+        currentPanel = searchPanel;
     }
 
     public void switchToFlight() {
-
+        currentPanel.setVisible(false);
+        flightListPanel.setVisible(true);
+        currentPanel = flightListPanel;
     }
 
     public void switchToSeat() {
-
+        currentPanel.setVisible(false);
+        seatPanel.setVisible(true);
+        currentPanel = seatPanel;
     }
 
     public void switchToInput() {
-
+        currentPanel.setVisible(false);
+        userInputPanel.setVisible(true);
+        currentPanel = userInputPanel;
     }
 
     public void switchToExport() {
-
+        currentPanel.setVisible(false);
+        exportPanel.setVisible(true);
+        currentPanel = exportPanel;
     }
 }
