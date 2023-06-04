68adc3a99243b1448b52152d4e5a8c9bcddbba24
agoalie <104106628+agoalie@users.noreply.github.com>
2023-06-03 21:37:52 -0400

    Planes

--- src/gui/graphics/Cloud.java
+++ src/gui/graphics/Cloud.java
@@ -19,17 +19,17 @@ public class Cloud {
     /**
      * The x-position of the cloud.
      */
-    private int xPos;
+    private int x;
 
     /**
      * The y-position of the cloud.
      */
-    private final int yPos;
+    private int y;
 
     /**
      * The type of the cloud.
      */
-    private final int type;
+    private final int type = (int) (Math.random() * 7);
 
     /**
      * The speed at which the cloud moves.
@@ -44,19 +44,19 @@ public class Cloud {
     /**
      * Constructs a Cloud object with the specified y-position.
      *
-     * @param yPos the y-position of the cloud
+     * @param y the y-position of the cloud
      * @author Aidan Baker
      */
-    public Cloud(int yPos) {
-        this.yPos = yPos;
+    public Cloud(int y) {
+        this.y = y;
+
         if ((int) (Math.random() * 2) == 1) {
             speed = (int) (Math.random() * 4) + 2;
-            xPos = -200;
+            x = -200;
         } else {
             speed = ((int) -(Math.random() * 4) + 2);
-            xPos = 1600;
+            x = 1600;
         }
-        type = (int) (Math.random() * 3);
     }
 
     /**
@@ -68,9 +68,9 @@ public class Cloud {
     private void drawType1(Graphics g) {
         g.setColor(WHITE);
 
-        g.fillOval(xPos + 20, yPos, 65, 35);
-        g.fillOval(xPos, yPos + 22, 75, 45);
-        g.fillOval(xPos + 45, yPos + 25, 70, 50);
+        g.fillOval(x + 20, y, 65, 35);
+        g.fillOval(x, y + 22, 75, 45);
+        g.fillOval(x + 45, y + 25, 70, 50);
     }
 
     /**
@@ -82,12 +82,12 @@ public class Cloud {
     private void drawType2(Graphics g) {
         g.setColor(WHITE);
 
-        g.fillOval(xPos, yPos + 10, 50, 50);
-        g.fillOval(xPos + 30, yPos, 50, 50);
-        g.fillOval(xPos + 60, yPos + 10, 50, 50);
+        g.fillOval(x, y + 10, 50, 50);
+        g.fillOval(x + 30, y, 50, 50);
+        g.fillOval(x + 60, y + 10, 50, 50);
 
-        g.fillOval(xPos + 20, yPos + 30, 50, 50);
-        g.fillOval(xPos + 50, yPos + 30, 50, 50);
+        g.fillOval(x + 20, y + 30, 50, 50);
+        g.fillOval(x + 50, y + 30, 50, 50);
     }
 
     /**
@@ -99,23 +99,13 @@ public class Cloud {
     private void drawType3(Graphics g) {
         g.setColor(WHITE);
 
-        g.fillOval(xPos, yPos + 10, 40, 40);
-        g.fillOval(xPos + 30, yPos, 60, 60);
-        g.fillOval(xPos + 70, yPos + 10, 40, 40);
+        g.fillOval(x, y + 10, 40, 40);
+        g.fillOval(x + 30, y, 60, 60);
+        g.fillOval(x + 70, y + 10, 40, 40);
 
-        g.fillOval(xPos + 10, yPos + 30, 30, 30);
-        g.fillOval(xPos + 60, yPos + 30, 30, 30);
-        g.fillOval(xPos + 30, yPos + 40, 40, 40);
-    }
-
-    /**
-     * A helper method to scale numbers to fit the screen properly.
-     *
-     * @param n The number to scale to fit the screen properly
-     * @return The scaled number
-     */
-    private int scale(int n) {
-        return n/10;
+        g.fillOval(x + 10, y + 30, 30, 30);
+        g.fillOval(x + 60, y + 30, 30, 30);
+        g.fillOval(x + 30, y + 40, 40, 40);
     }
 
     /**
@@ -124,10 +114,147 @@ public class Cloud {
      * @author Aidan Baker
      */
     private void drawPlane(Graphics g) {
-        Boolean isFacingRight = speed > 0;
+        final Color BLACK = Color.BLACK;
+        final Color GRAY = Color.LIGHT_GRAY;
 
-        g.setColor(new Color(48, 77, 173));
-        g.fillOval(xPos+scale(16), yPos+scale(368), scale(254), scale(285));
+        if (speed < 0) {
+            //wings
+            int[] topWingX = {x+117, x+128, x+139, x+133};
+            int[] topWingY = {63-y, 36-y, 35-y, 61-y};
+            int[] bottomWingX = {x+117, x+132, x+143, x+133};
+            int[] bottomWingY = {79-y, 106-y, 107-y, 81-y};
+
+            //tail fins
+            int[] mainTailFinX = {x+180, x+203, x+213, x+190};
+            int[] mainTailFinY = {65-y, 48-y, 50-y, 73-y};
+            int[] bottomTailFinX = {x+182, x+199, x+204, x+190};
+            int[] bottomTailFinY = {79-y, 88-y, 85-y, 73-y};
+            int[] topTailFinX = {x+178, x+186, x+190, x+189};
+            int[] topTailFinY = {64-y, 51-y, 51-y, 67-y};
+
+            g.setColor(GRAY);
+            g.fillPolygon(topWingX, topWingY, 4);
+            g.setColor(BLACK);
+            g.drawPolygon(topWingX, topWingY, 4);
+
+            //body of plane
+            g.setColor(GRAY);
+            g.fillOval(x+90, 60-y, 100, 25);
+            g.setColor(BLACK);
+            g.drawOval(x+90, 60-y, 100, 25);
+
+            //top tail fin
+            g.setColor(GRAY);
+            g.fillPolygon(topTailFinX, topTailFinY, 4);
+            g.setColor(BLACK);
+            g.drawPolygon(topTailFinX, topTailFinY, 4);
+
+            //main and bottom tail fin
+            g.setColor(GRAY);
+            g.fillPolygon(mainTailFinX, mainTailFinY, 4);
+            g.fillPolygon(bottomTailFinX, bottomTailFinY, 4);
+            g.setColor(BLACK);
+            g.drawPolygon(mainTailFinX, mainTailFinY, 4);
+            g.drawPolygon(bottomTailFinX, bottomTailFinY, 4);
+
+            //cockpit
+            g.setColor(Color.cyan);
+            g.fillArc(x+90, 65-y, 33, 15, 90, 90);
+            g.setColor(BLACK);
+            g.drawArc(x+90, 65-y, 33, 15, 90, 90);
+            g.drawLine(x+106, 66-y, x+106, 73-y);
+            g.drawLine(x+91, 73-y, x+106, 73-y);
+
+            //bottom wing
+            g.setColor(GRAY);
+            g.fillPolygon(bottomWingX, bottomWingY, 4);
+            g.setColor(BLACK);
+            g.drawPolygon(bottomWingX, bottomWingY, 4);
+
+            //windows
+            g.setColor(Color.cyan);
+            g.fillRoundRect(x+120, 67-y, 5, 7, 4, 4);
+            g.fillRoundRect(x+130, 67-y, 5, 7, 4, 4);
+            g.fillRoundRect(x+140, 67-y, 5, 7, 4, 4);
+            g.fillRoundRect(x+150, 67-y, 5, 7, 4, 4);
+            g.fillRoundRect(x+160, 67-y, 5, 7, 4, 4);
+
+            g.setColor(BLACK);
+            g.drawRoundRect(x+120, 67-y, 5, 7, 4, 4);
+            g.drawRoundRect(x+130, 67-y, 5, 7, 4, 4);
+            g.drawRoundRect(x+140, 67-y, 5, 7, 4, 4);
+            g.drawRoundRect(x+150, 67-y, 5, 7, 4, 4);
+            g.drawRoundRect(x+160, 67-y, 5, 7, 4, 4);
+        }
+        else {
+            // Wings
+            int[] topWingX = {x+183, x+172, x+161, x+167};
+            int[] topWingY = {63-y, 36-y, 35-y, 61-y};
+            int[] bottomWingX = {x+183, x+168, x+157, x+167};
+            int[] bottomWingY = {79-y, 106-y, 107-y, 81-y};
+
+            // Tail fins
+            int[] mainTailFinX = {x+120, x+97, x+87, x+110};
+            int[] mainTailFinY = {65-y, 48-y, 50-y, 73-y};
+            int[] bottomTailFinX = {x+118, x+101, x+96, x+110};
+            int[] bottomTailFinY = {79-y, 88-y, 85-y, 73-y};
+            int[] topTailFinX = {x+122, x+114, x+110, x+111};
+            int[] topTailFinY = {64-y, 51-y, 51-y, 67-y};
+
+            g.setColor(GRAY);
+            g.fillPolygon(topWingX, topWingY, 4);
+            g.setColor(BLACK);
+            g.drawPolygon(topWingX, topWingY, 4);
+
+            // Body of plane
+            g.setColor(GRAY);
+            g.fillOval(x+110, 60-y, 100, 25);
+            g.setColor(BLACK);
+            g.drawOval(x+110, 60-y, 100, 25);
+
+            // Top tail fin
+            g.setColor(GRAY);
+            g.fillPolygon(topTailFinX, topTailFinY, 4);
+            g.setColor(BLACK);
+            g.drawPolygon(topTailFinX, topTailFinY, 4);
+
+            // Main and bottom tail fin
+            g.setColor(GRAY);
+            g.fillPolygon(mainTailFinX, mainTailFinY, 4);
+            g.fillPolygon(bottomTailFinX, bottomTailFinY, 4);
+            g.setColor(BLACK);
+            g.drawPolygon(mainTailFinX, mainTailFinY, 4);
+            g.drawPolygon(bottomTailFinX, bottomTailFinY, 4);
+
+            // Cockpit
+            g.setColor(Color.CYAN);
+            g.fillArc(x+177, 65-y, 33, 15, 0, 90);
+            g.setColor(BLACK);
+            g.drawArc(x+177, 65-y, 33, 15, 0, 90);
+            g.drawLine(x+193, 66-y, x+193, 73-y);
+            g.drawLine(x+193, 73-y, x+208, 73-y);
+
+            // Bottom wing
+            g.setColor(GRAY);
+            g.fillPolygon(bottomWingX, bottomWingY, 4);
+            g.setColor(BLACK);
+            g.drawPolygon(bottomWingX, bottomWingY, 4);
+
+            // Windows
+            g.setColor(Color.CYAN);
+            g.fillRoundRect(x+180, 67-y, 5, 7, 4, 4);
+            g.fillRoundRect(x+170, 67-y, 5, 7, 4, 4);
+            g.fillRoundRect(x+160, 67-y, 5, 7, 4, 4);
+            g.fillRoundRect(x+150, 67-y, 5, 7, 4, 4);
+            g.fillRoundRect(x+140, 67-y, 5, 7, 4, 4);
+
+            g.setColor(BLACK);
+            g.drawRoundRect(x+180, 67-y, 5, 7, 4, 4);
+            g.drawRoundRect(x+170, 67-y, 5, 7, 4, 4);
+            g.drawRoundRect(x+160, 67-y, 5, 7, 4, 4);
+            g.drawRoundRect(x+150, 67-y, 5, 7, 4, 4);
+            g.drawRoundRect(x+140, 67-y, 5, 7, 4, 4);
+        }
     }
 
     /**
@@ -136,7 +263,11 @@ public class Cloud {
      * @author Aidan Baker
      */
     public void move() {
-        xPos += speed;
+        x += speed;
+
+        if (type == 7 || type == 0) {
+            y = (int) (Math.sin(x/50.0) * 15);
+        }
     }
 
     /**
@@ -146,8 +277,8 @@ public class Cloud {
      * @author Aidan Baker
      */
     public boolean isOverScreen() {
-        if (speed > 0) return xPos > 1400;
-        else return xPos < -200;
+        if (speed > 0) return x > 1400;
+        else return x < -200;
     }
 
     /**
@@ -158,14 +289,17 @@ public class Cloud {
      */
     public void paint(Graphics g) {
         switch (type) {
-            case 1:
+            case 1, 2:
                 drawType1(g);
                 break;
-            case 2:
+            case 3, 4:
                 drawType2(g);
                 break;
-            default:
+            case 5, 6:
                 drawType3(g);
+                break;
+            default:
+                drawPlane(g);
                 break;
         }
     }
